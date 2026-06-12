b436879fe535f81c6faeecd155d95e85ac323abd
balajis-brigosha <balaji.s@brigosha.com>
2026-06-12 17:29:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ while True:
     with open("/tmp/heartbeat.txt", "w") as f:
         f.write(str(time.time()))
 
-    print("Running Version 12")
+    print("Running Version 13.0")
 
     time.sleep(3)
