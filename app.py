16371f44ad594fdb4a5f8c2ddd849d436c8a53c0
balajis-brigosha <balaji.s@brigosha.com>
2026-06-16 11:52:13 +0530

    Update version number from 19 to 20

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ while True:
     with open("/tmp/heartbeat.txt", "w") as f:
         f.write(str(time.time()))
 
-    print("Running Version 19")
+    print("Running Version 20")
 
     time.sleep(3)
